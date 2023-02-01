6c08bb6b99455a63b5e22b636f0ae63696cf76a4
valeriiosinnii <valeriiosiniiost@gmail.com>
2023-02-01 12:42:38 +0100

    Updated argparse

--- wuff.py
+++ wuff.py
@@ -1,26 +1,29 @@
 import argparse
+
 from wuff_find import search
 from wuff_create import create_new_dog
 from wuff_stats import stats
+
 
 def run(args):
     if args.find:
         search(args.find)
     elif args.create:
-        if args.outputdir:
-            create_new_dog(output=args.outputdir)
+        if args.output:
+            create_new_dog(output=args.output)
         else:
-            create_new_dog(output='/Users/rev4l/')
+            create_new_dog(output='C:\\Users\\Public')
     elif args.stats:
         stats()
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='Dog data around Zurich')
-    parser.add_argument('find', help='Search for dogs', nargs="?")
+    parser = argparse.ArgumentParser(description='Providing information about dogs in Zurich')
     parser.add_argument('stats', help='Stats about dogs', action='store_true')
-    parser.add_argument('--create', help='Create a new dog', action='store_true')
-    parser.add_argument('--outputdir', '-o', help='Output directory(With --create usage only)', nargs="?")
+    parser.add_argument('-f', '--find', help='Search for dogs', action='store_true')
+    parser.add_argument('-c', '--create', help='Create a new dog', action='store_true')
+    parser.add_argument('-o', '--output', help='Output directory')
+
     return parser
 
 
